d0f5c90a6e0d9dd34259932a959c63a06b1bd54b
Fernando Calisaya <legasy23dream@gmail.com>
2025-04-26 20:28:50 -0300

    tabla-usuarios-creada

--- NOC-Usuarios.sql
+++ NOC-Usuarios.sql
@@ -1,9 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
 -- Servidor: localhost:3307
--- Tiempo de generación: 24-04-2025 a las 01:31:44
+-- Tiempo de generación: 27-04-2025 a las 00:00:50
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -26,27 +22,9 @@ USE `NOC`;
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `Alumnos`
---
-
-CREATE TABLE `Alumnos` (
-  `id_alumno` int(11) NOT NULL,
-  `nombre` varchar(100) DEFAULT NULL,
-  `apellido` varchar(100) DEFAULT NULL,
-  `fecha_nacimiento` date DEFAULT NULL,
-  `direccion` varchar(255) DEFAULT NULL,
-  `telefono` varchar(15) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- RELACIONES PARA LA TABLA `Alumnos`:
---
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `Asistencia`
+--
+-- Creación: 23-04-2025 a las 02:52:59
 --
 
 CREATE TABLE `Asistencia` (
@@ -67,6 +45,8 @@ CREATE TABLE `Asistencia` (
 --
 -- Estructura de tabla para la tabla `CondicionesAprobacion`
 --
+-- Creación: 23-04-2025 a las 02:52:59
+--
 
 CREATE TABLE `CondicionesAprobacion` (
   `id_condicion` int(11) NOT NULL,
@@ -86,6 +66,8 @@ CREATE TABLE `CondicionesAprobacion` (
 --
 -- Estructura de tabla para la tabla `CursoCredito`
 --
+-- Creación: 23-04-2025 a las 02:52:59
+--
 
 CREATE TABLE `CursoCredito` (
   `id_curso` int(11) NOT NULL,
@@ -104,6 +86,8 @@ CREATE TABLE `CursoCredito` (
 
 --
 -- Estructura de tabla para la tabla `Cursos`
+--
+-- Creación: 23-04-2025 a las 02:52:59
 --
 
 CREATE TABLE `Cursos` (
@@ -125,53 +109,28 @@ CREATE TABLE `Cursos` (
 --
 -- Estructura de tabla para la tabla `DocenteCurso`
 --
+-- Creación: 26-04-2025 a las 21:59:39
+--
 
 CREATE TABLE `DocenteCurso` (
-  `id_docente` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
   `id_curso` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- RELACIONES PARA LA TABLA `DocenteCurso`:
---   `id_docente`
---       `Docentes` -> `id_docente`
 --   `id_curso`
 --       `Cursos` -> `id_curso`
+--   `id_usuario`
+--       `Usuarios` -> `id_usuario`
 --
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `Docentes`
---
-
-CREATE TABLE `Docentes` (
-  `id_docente` int(11) NOT NULL,
-  `docenteName` varchar(100) DEFAULT NULL,
-  `docenteLastName` varchar(100) DEFAULT NULL,
-  `docenteEspeciality` varchar(100) DEFAULT NULL,
-  `docentePassword` varchar(150) NOT NULL,
-  `docenteEmail` varchar(100) DEFAULT NULL,
-  `docenteTelefone` bigint(11) DEFAULT NULL,
-  `docenteDni` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- RELACIONES PARA LA TABLA `Docentes`:
---
-
---
--- Volcado de datos para la tabla `Docentes`
---
-
-INSERT INTO `Docentes` (`id_docente`, `docenteName`, `docenteLastName`, `docenteEspeciality`, `docentePassword`, `docenteEmail`, `docenteTelefone`, `docenteDni`) VALUES
-(1, 'Juana', 'Castaño', 'Biologa', 'juanabsas2025', 'juana', 3114896324, 28654987),
-(2, 'Juan', 'Castillo', 'Matematica', 'juan200py', 'Juan2000ag@gmail.com', 3114896555, 29698987);
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `Empresas`
+--
+-- Creación: 23-04-2025 a las 02:52:59
 --
 
 CREATE TABLE `Empresas` (
@@ -191,6 +150,8 @@ CREATE TABLE `Empresas` (
 
 --
 -- Estructura de tabla para la tabla `EntrenamientosLaborales`
+--
+-- Creación: 23-04-2025 a las 02:52:59
 --
 
 CREATE TABLE `EntrenamientosLaborales` (
@@ -213,20 +174,22 @@ CREATE TABLE `EntrenamientosLaborales` (
 --
 -- Estructura de tabla para la tabla `Inscripciones`
 --
+-- Creación: 26-04-2025 a las 21:59:12
+--
 
 CREATE TABLE `Inscripciones` (
   `id_inscripcion` int(11) NOT NULL,
-  `id_alumno` int(11) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
   `id_curso` int(11) DEFAULT NULL,
   `fecha_inscripcion` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- RELACIONES PARA LA TABLA `Inscripciones`:
---   `id_alumno`
---       `Alumnos` -> `id_alumno`
 --   `id_curso`
 --       `Cursos` -> `id_curso`
+--   `id_usuario`
+--       `Usuarios` -> `id_usuario`
 --
 
 -- --------------------------------------------------------
@@ -234,10 +197,12 @@ CREATE TABLE `Inscripciones` (
 --
 -- Estructura de tabla para la tabla `Pagos`
 --
+-- Creación: 26-04-2025 a las 21:59:12
+--
 
 CREATE TABLE `Pagos` (
   `id_pago` int(11) NOT NULL,
-  `id_alumno` int(11) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
   `id_curso` int(11) DEFAULT NULL,
   `monto` decimal(10,2) DEFAULT NULL,
   `fecha_pago` date DEFAULT NULL
@@ -245,16 +210,18 @@ CREATE TABLE `Pagos` (
 
 --
 -- RELACIONES PARA LA TABLA `Pagos`:
---   `id_alumno`
---       `Alumnos` -> `id_alumno`
 --   `id_curso`
 --       `Cursos` -> `id_curso`
+--   `id_usuario`
+--       `Usuarios` -> `id_usuario`
 --
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `ProgramasCredito`
+--
+-- Creación: 23-04-2025 a las 02:52:59
 --
 
 CREATE TABLE `ProgramasCredito` (
@@ -275,6 +242,8 @@ CREATE TABLE `ProgramasCredito` (
 --
 -- Estructura de tabla para la tabla `Publicaciones`
 --
+-- Creación: 23-04-2025 a las 02:52:59
+--
 
 CREATE TABLE `Publicaciones` (
   `id_publicacion` int(11) NOT NULL,
@@ -289,15 +258,35 @@ CREATE TABLE `Publicaciones` (
 -- RELACIONES PARA LA TABLA `Publicaciones`:
 --
 
+-- --------------------------------------------------------
+
 --
--- Índices para tablas volcadas
+-- Estructura de tabla para la tabla `Usuarios`
+--
+-- Creación: 26-04-2025 a las 21:50:54
+--
+
+CREATE TABLE `Usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `apellido` varchar(100) DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `telefono` varchar(15) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(150) NOT NULL,
+  `dni` int(11) DEFAULT NULL,
+  `especialidad` varchar(100) DEFAULT NULL,
+  `tipo_usuario` enum('alumno','docente') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- RELACIONES PARA LA TABLA `Usuarios`:
 --
 
 --
--- Indices de la tabla `Alumnos`
+-- Índices para tablas volcadas
 --
-ALTER TABLE `Alumnos`
-  ADD PRIMARY KEY (`id_alumno`);
 
 --
 -- Indices de la tabla `Asistencia`
@@ -330,14 +319,8 @@ ALTER TABLE `Cursos`
 -- Indices de la tabla `DocenteCurso`
 --
 ALTER TABLE `DocenteCurso`
-  ADD PRIMARY KEY (`id_docente`,`id_curso`),
+  ADD PRIMARY KEY (`id_usuario`,`id_curso`),
   ADD KEY `id_curso` (`id_curso`);
-
---
--- Indices de la tabla `Docentes`
---
-ALTER TABLE `Docentes`
-  ADD PRIMARY KEY (`id_docente`);
 
 --
 -- Indices de la tabla `Empresas`
@@ -357,16 +340,16 @@ ALTER TABLE `EntrenamientosLaborales`
 --
 ALTER TABLE `Inscripciones`
   ADD PRIMARY KEY (`id_inscripcion`),
-  ADD KEY `id_alumno` (`id_alumno`),
-  ADD KEY `id_curso` (`id_curso`);
+  ADD KEY `id_curso` (`id_curso`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indices de la tabla `Pagos`
 --
 ALTER TABLE `Pagos`
   ADD PRIMARY KEY (`id_pago`),
-  ADD KEY `id_alumno` (`id_alumno`),
-  ADD KEY `id_curso` (`id_curso`);
+  ADD KEY `id_curso` (`id_curso`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indices de la tabla `ProgramasCredito`
@@ -381,14 +364,14 @@ ALTER TABLE `Publicaciones`
   ADD PRIMARY KEY (`id_publicacion`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- Indices de la tabla `Usuarios`
 --
+ALTER TABLE `Usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
 
 --
--- AUTO_INCREMENT de la tabla `Alumnos`
+-- AUTO_INCREMENT de las tablas volcadas
 --
-ALTER TABLE `Alumnos`
-  MODIFY `id_alumno` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `Asistencia`
@@ -407,12 +390,6 @@ ALTER TABLE `CondicionesAprobacion`
 --
 ALTER TABLE `Cursos`
   MODIFY `id_curso` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `Docentes`
---
-ALTER TABLE `Docentes`
-  MODIFY `id_docente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `Empresas`
@@ -451,6 +428,12 @@ ALTER TABLE `Publicaciones`
   MODIFY `id_publicacion` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `Usuarios`
+--
+ALTER TABLE `Usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- Restricciones para tablas volcadas
 --
 
@@ -477,8 +460,8 @@ ALTER TABLE `CursoCredito`
 -- Filtros para la tabla `DocenteCurso`
 --
 ALTER TABLE `DocenteCurso`
-  ADD CONSTRAINT `DocenteCurso_ibfk_1` FOREIGN KEY (`id_docente`) REFERENCES `Docentes` (`id_docente`),
-  ADD CONSTRAINT `DocenteCurso_ibfk_2` FOREIGN KEY (`id_curso`) REFERENCES `Cursos` (`id_curso`);
+  ADD CONSTRAINT `DocenteCurso_ibfk_2` FOREIGN KEY (`id_curso`) REFERENCES `Cursos` (`id_curso`),
+  ADD CONSTRAINT `DocenteCurso_ibfk_3` FOREIGN KEY (`id_usuario`) REFERENCES `Usuarios` (`id_usuario`);
 
 --
 -- Filtros para la tabla `EntrenamientosLaborales`
@@ -490,15 +473,15 @@ ALTER TABLE `EntrenamientosLaborales`
 -- Filtros para la tabla `Inscripciones`
 --
 ALTER TABLE `Inscripciones`
-  ADD CONSTRAINT `Inscripciones_ibfk_1` FOREIGN KEY (`id_alumno`) REFERENCES `Alumnos` (`id_alumno`),
-  ADD CONSTRAINT `Inscripciones_ibfk_2` FOREIGN KEY (`id_curso`) REFERENCES `Cursos` (`id_curso`);
+  ADD CONSTRAINT `Inscripciones_ibfk_2` FOREIGN KEY (`id_curso`) REFERENCES `Cursos` (`id_curso`),
+  ADD CONSTRAINT `Inscripciones_ibfk_3` FOREIGN KEY (`id_usuario`) REFERENCES `Usuarios` (`id_usuario`);
 
 --
 -- Filtros para la tabla `Pagos`
 --
 ALTER TABLE `Pagos`
-  ADD CONSTRAINT `Pagos_ibfk_1` FOREIGN KEY (`id_alumno`) REFERENCES `Alumnos` (`id_alumno`),
-  ADD CONSTRAINT `Pagos_ibfk_2` FOREIGN KEY (`id_curso`) REFERENCES `Cursos` (`id_curso`);
+  ADD CONSTRAINT `Pagos_ibfk_2` FOREIGN KEY (`id_curso`) REFERENCES `Cursos` (`id_curso`),
+  ADD CONSTRAINT `Pagos_ibfk_3` FOREIGN KEY (`id_usuario`) REFERENCES `Usuarios` (`id_usuario`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
