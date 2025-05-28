1406ccccdd299ae720554434ee7a919de2bb9239
NaNajli <nadurodriguez2@gmail.com>
2025-05-28 10:58:47 -0300

    ultimos cambios

--- controllers/userscontrollers.js
+++ controllers/userscontrollers.js
@@ -184,12 +184,17 @@ const userLogin = async (req, res) => {
 //***************************************************************
 const LoginconGithub = async (req, res) => {
    if (req.session && req.session.user) {
-    const usuario = {
-      nombre: req.session.user.displayName,//nota github devuenlve nombre completo no nombre y apellido deveria modificar tabla
-      email: req.session.user.emails?.[0]?.value || 'Email no disponible',
-      foto: req.session.user.photos?.[0]?.value || null
-    };
+   
+    const [apellido, ...rest] = req.user.displayName.trim().split(' ');
+    const nombre = rest.join(' ');
 
+    req.session.user = {
+      user_id: req.user.id,
+      nombre,
+      apellido,
+      email: req.user.emails?.[0]?.value || '',
+      foto: req.user.photos?.[0]?.value || ''
+    }
     return res.status(200).json({ usuario });
   } else {
     return res.status(401).json({ message: 'No hay sesión activa' });
